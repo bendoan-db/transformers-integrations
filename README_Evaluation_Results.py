374144c4eec003962b13312c7bd5e7d010049d3f
tian.tan@databricks.com <tian.tan@databricks.com>
2023-08-18 18:04:33 +0000

    Update with PEFT results and prompt evaluation

--- README_Evaluation_Results.py
+++ README_Evaluation_Results.py
@@ -19,7 +19,7 @@
 # MAGIC
 # MAGIC |       | **Full Weight** | **PEFT**     |
 # MAGIC | :---        |    :----:   |          ---: |
-# MAGIC | **Single-GPU**      | TBD       | Training: ~4 hours // Batch Inference: ~2.45 hours   |
+# MAGIC | **Single-GPU**      | TBD       | Training: ~4 hours // Batch Inference (800 records): ~2.65 hours   |
 # MAGIC | **Multi-GPU**   | 10+ hours then OOM       | TBD      |
 # MAGIC
 # MAGIC
@@ -28,12 +28,60 @@
 # MAGIC
 # MAGIC |       | **Full Weight** | **PEFT**     |
 # MAGIC | :---        |    :----:   |          ---: |
-# MAGIC | **Single-GPU**      | XX       | XX   |
-# MAGIC | **Multi-GPU**   | XX        | XX      |
+# MAGIC | **Single-GPU**      | TBD       | Training:    |
+# MAGIC | **Multi-GPU**   | TBD        | TBD      |
 # MAGIC
 # MAGIC
 # MAGIC
 # MAGIC ##### Prompt Evaluation
+
+# COMMAND ----------
+
+# DBTITLE 0,Install Libraries
+!pip install --upgrade pip
+!pip install -q bitsandbytes datasets accelerate loralib
+!pip install -q git+https://github.com/huggingface/transformers.git@main git+https://github.com/huggingface/peft.git
+!pip install accelerate==0.21.0
+!pip install transformers==4.31.0
+!pip install -U "fsspec[http]>=2021.05.0"
+!pip install py7zr
+
+# COMMAND ----------
+
+# DBTITLE 1,Load model from the Hub
+import torch
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+peft_model_id = "tiantan32/t5-3b-samsum"
+config = PeftConfig.from_pretrained(peft_model_id)
+model = AutoModelForSeq2SeqLM.from_pretrained(
+            config.base_model_name_or_path, 
+            load_in_8bit=True, 
+            device_map='auto'
+            )
+tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+
+# Load the Lora model
+model = PeftModel.from_pretrained(model, peft_model_id)
+
+# COMMAND ----------
+
+# DBTITLE 1,Prompt Evaluation Example
+from datasets import load_dataset 
+from random import randrange
+
+
+# Load dataset from the hub and get a sample
+dataset = load_dataset("samsum")
+sample = dataset['test'][randrange(len(dataset["test"]))]
+
+input_ids = tokenizer(sample["dialogue"], return_tensors="pt", truncation=True).input_ids.cuda()
+# with torch.inference_mode():
+outputs = model.generate(input_ids=input_ids, max_new_tokens=10, do_sample=True, top_p=0.9)
+print(f"input sentence: {sample['dialogue']}\n{'---'* 20}")
+
+print(f"summary:\n{tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0]}")
 
 # COMMAND ----------
 
